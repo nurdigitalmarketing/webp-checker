efa54708b6337ed055aea2a9f2a721addce8409d
NUR® Digital Marketing <163868030+nurdigitalmarketing@users.noreply.github.com>
2024-04-22 17:15:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin  # Importa urljoin per normalizzare gli URL
+from urllib.parse import urljoin, urlparse  # Importa anche urlparse per analizzare gli URL
 
 def fetch_images(url):
     """ Recupera le immagini dall'URL indicato e verifica i formati, restituendo un dizionario delle categorie con le rispettive immagini. """
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     images = soup.find_all('img')
+    
+    # Analizza l'URL per ottenere una base corretta che termina con uno slash
+    parsed_url = urlparse(url)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}/"
     
     images_dict = {
         'webp': [],
@@ -18,7 +22,7 @@ def fetch_images(url):
     for img in images:
         src = img.get('src', '')
         # Normalizza l'URL unendo il dominio se l'URL dell'immagine è relativo
-        full_url = urljoin(url, src)
+        full_url = urljoin(base_url, src)  # Usa base_url che termina con slash
         
         if full_url.endswith('.webp'):
             images_dict['webp'].append(full_url)
